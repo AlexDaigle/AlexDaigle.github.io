4f6daf9a012fbd2d1e3839f92c4c1c8ddd935374
AlexDaigle <daigle637@wearescpps.org>
2025-09-05 19:09:32 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
-
+    toggleGrid();
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(260, 640, 100, 10, "grey")
+createPlatform(50, 0, 10, 600, "grey")
+createPlatform(100, 0, 10, 600, "grey")
+createPlatform(100, 530, 100, 10, "grey")
+createPlatform(500, 580, 100, 10, "grey")
+createPlatform(700, 500, 100, 10, "grey")
+createPlatform(1000,500, 100, 10, "grey")
+createPlatform(500, 400, 100, 10, "grey")
+createPlatform(300, 330, 50, 10, "grey")
+createPlatform(500, 250, 100, 10, "grey")
+createPlatform(700, 250, 100, 10, "grey")
+createPlatform(900, 250, 100, 10, "grey")
+createPlatform(1100, 250, 100, 10, "grey")
+createPlatform(1300, 250, 100, 10, "grey")
 
     // TODO 3 - Create Collectables
-
-
-
+createCollectable("database", 130, 480, .03, 1 )
+createCollectable("database", 1030, 450, .03, 1 )
+createCollectable("database", 530, 200, .03, 1)
+createCollectable("database", 1330, 200, .03, 1)
     
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("top", 500, 1050)
+createCannon("bottom", 800, 2000,)
+createCannon("top", 1300, 1500)
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
